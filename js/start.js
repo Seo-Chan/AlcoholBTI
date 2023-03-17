20d3bca5b43257f4f67590bd2086f11b18081cf9
seoyeon <wjddowkd456@naver.com>
2023-03-18 00:18:04 +0900

    feat: 결과 페이지 기능 구현

--- js/start.js
+++ js/start.js
@@ -4,6 +4,42 @@ const result = document.querySelector("#result");
 const endPoint = 12;
 const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+function calResult() {
+  var result = select.indexOf(Math.max(...select));
+  return result;
+}
+
+function setResult() {
+  let point = calResult();
+  const resultName = document.querySelector(".resultName");
+  resultName.innerHTML = resultList[point].name;
+
+  var resultImg = document.createElement("img");
+  const imgDiv = document.querySelector("#resultImg");
+  var imgURL = "img/image-" + point + ".png";
+  resultImg.src = imgURL;
+  resultImg.alt = point;
+  resultImg.classList.add("img-fluid");
+  imgDiv.appendChild(resultImg);
+
+  const resultDesc = document.querySelector(".resultDesc");
+  resultDesc.innerHTML = resultList[point].desc;
+}
+
+function goResult() {
+  qna.style.WebkitAnimation = "fadeOut 1s";
+  qna.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    result.style.WebkitAnimation = "fadeIn 1s";
+    result.style.animation = "fadeIn 1s";
+    setTimeout(() => {
+      qna.style.display = "none";
+      result.style.display = "block";
+    }, 450);
+  });
+  setResult();
+}
+
 function addAnswer(answerTxt, qnaIdx, idx) {
   var answerBox = document.querySelector(".answerBox");
   var answer = document.createElement("button");
@@ -11,25 +47,33 @@ function addAnswer(answerTxt, qnaIdx, idx) {
   answerBox.appendChild(answer);
   answer.innerHTML = answerTxt;
 
-  answer.addEventListener("click", function () {
-    var children = document.querySelectorAll(".answerList");
-    for (let i = 0; i < children.length; i++) {
-      children[i].disabled = true;
-    }
-    setTimeout(() => {
-      var target = qnaList[qnaIdx].a[idx].type;
-      for (let j = 0; j < target.length; j++) {
-        select[target[j]] += 1;
-      }
+  answer.addEventListener(
+    "click",
+    function () {
+      var children = document.querySelectorAll(".answerList");
       for (let i = 0; i < children.length; i++) {
-        children[i].style.display = "none";
+        children[i].disabled = true;
       }
-      goNext(++qnaIdx);
-    }, 200);
-  });
+      setTimeout(() => {
+        var target = qnaList[qnaIdx].a[idx].type;
+        for (let j = 0; j < target.length; j++) {
+          select[target[j]] += 1;
+        }
+        for (let i = 0; i < children.length; i++) {
+          children[i].style.display = "none";
+        }
+        goNext(++qnaIdx);
+      }, 200);
+    },
+    false
+  );
 }
 
 function goNext(qnaIdx) {
+  if (qnaIdx === endPoint) {
+    goResult();
+    return;
+  }
   var qna = document.querySelector(".qnaBox");
   qna.innerHTML = qnaList[qnaIdx].q;
   for (let i in qnaList[qnaIdx].a) {
